ccb7d830112e838ff1963cef54b064aef91f9cb5
VanessaBalamUwU <109107632+VanessaBalamUwU@users.noreply.github.com>
2023-12-05 23:32:30 -0500

    #994

---
+++ hola_mundo_con_variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+Nombre=""
+
+echo -n "Ingresa tu nombre: "
+read Nombre
+
+echo -e "\033[40m\033[36m Este Script dice:  \033[0m"
+echo -e "\033[40m\033[35m \n¡Hola! ${Nombre} \033[0m"
--- menu.sh
+++ menu.sh
@@ -1,1 +1,81 @@
-https://asciinema.org/a/kbPRRN5f791zuMX5MQPTj62jh
+#!/bin/bash 
+SCRIPT=""
+clear
+echo "  
+ 
+▒█▀▄▀█ █▀▀ █▀▀▄ █░░█ 
+▒█▒█▒█ █▀▀ █░░█ █░░█ 
+▒█░░▒█ ▀▀▀ ▀░░▀ ░▀▀▀
+ "
+ 
+while true
+do
+	echo "1.- Arbol"
+	echo "2.- Hola mundo"
+	echo "3.- Hola nombre"
+	echo "4.- Variables"
+	echo "5.- Arrays"
+	echo "6.- Arrays 2"
+	echo "7.- Aritmeticas"
+	echo "8.- Logicas"
+	echo "9.- Condicionales"
+	echo "10.- Comprobaciones"
+	echo "11.- Case"
+	echo "12.- Iteraciones"
+	echo "13.- Iteracion while"
+	echo "14.- Iteracion Until"
+	echo "15.- Iteracion Select"
+	echo "16.- Funciones"
+	echo "17.- Libreria"
+	echo "18.- Señales"
+	echo "19.- Colores"	
+	echo "x.- Salir"
+
+	echo -n "Selecciona un script:" 
+
+	read SCRIPT
+
+	case ${SCRIPT} in 
+		1)	source arbol.sh
+			;;
+		2)	source hola_mundo.sh
+			;;
+		3) 	source hola_mundo_con_variables.sh
+			;;
+		4) 	source variables.sh
+			;;
+		5) 	source arrays.sh
+			;;
+		6) 	source arrays2.sh
+			;;
+		7) 	source Aritmeticas.sh
+			;;
+		8) 	source Logicas.sh
+			;;
+		9) 	source Condicionales.sh
+			;;
+		10) 	source Comprobaciones.sh
+			;;
+		11) 	source case.sh
+			;;
+		12) 	source Iteraciones.sh
+			;;
+		13) 	source Iteracion_While.sh
+			;;
+		14) 	source Iteracion_Until.sh
+			;;
+		15) 	source Iteracion_Select.sh
+			;;
+		16) 	source Funciones.sh
+			;;
+		17) 	source Libreria.sh
+			;;
+		18) 	source Señales.sh
+			;;
+		19) 	source Colores.sh
+			;;
+		20) 	break
+			;;
+		*)	echo "esa opcion no existe"
+	esac
+done 
